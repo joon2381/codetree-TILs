f16987f2d0e495bbafe1a6b07943baa414400a12
joon2381 <53505141+joon2381@users.noreply.github.com>
2024-08-06 20:14:47 +0900

    Title: 비교 연산, Time: 117ms, Memory: 24MB, Status: Passed - CodeTree

---
+++ 240806/비교 연산/comparison-operator.py
@@ -0,0 +1,8 @@
+a, b = map(int, input().split())
+
+print("%d" % 1 if a >= b else 0)
+print("%d" % 1 if a>b else 0)
+print("%d" % 1 if b>=a else 0)
+print("%d" % 1 if b>a else 0)
+print("%d" % 1 if a==b else 0)
+print("%d" % 1 if a!=b else 0)
