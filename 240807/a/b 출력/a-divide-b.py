cb482ae6d959b06725f74ac41edb41c14cbe6957
joon2381 <53505141+joon2381@users.noreply.github.com>
2024-08-07 20:24:50 +0900

    Title: a/b 출력, Time: 111ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240807/a/b 출력/a-divide-b.py
@@ -0,0 +1,12 @@
+a, b = map(int, input().split())
+cnt = 0
+for _ in range(21):
+    c = a//b
+    print(c, end="")
+    if cnt == 0 and c//10 == 0:
+        print('.', end="")
+        cnt += 1
+    if a < b:
+        a = a*10 - b*c
+    else:
+        a = a - (b * c)
