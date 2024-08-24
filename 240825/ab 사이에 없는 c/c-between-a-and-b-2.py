b59b9da3c2a7c6baae1178e183ead16367b21153
joon2381 <53505141+joon2381@users.noreply.github.com>
2024-08-25 00:50:32 +0900

    Title: ab 사이에 없는 c, Time: 143ms, Memory: 24MB, Status: Passed - CodeTree

--- 240825/ab 사이에 없는 c/c-between-a-and-b-2.py
+++ 240825/ab 사이에 없는 c/c-between-a-and-b-2.py
@@ -3,7 +3,7 @@ a, b, c = map(int, input().split())
 guess = True
 
 for i in range(1, (b//c)+1):
-    if a >= c * i and b <= c * i:
+    if a <= c * i and b >= c * i:
         guess = False
         break
 
